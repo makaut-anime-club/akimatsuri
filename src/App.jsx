e7ffe4f2cd5e3ab3117c9763ba566e6f9b16fe0c
Subha-5 <subhasadhu5@gmail.com>
2023-07-17 02:01:03 +0530

    added pika-face

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Lottie from 'react-lottie';
+
 import './App.css'
 import About from './components/About';
 import Events from './components/Events';
 import Home from './components/Home';
 import Stall from './components/Stall';
 import RootLayout from './pages/Root';
+import * as pika from "./json/pikachu.json";
+import { useEffect, useState } from 'react';
 
+const defaultOption1 = {
+  loop: true,
+  autoplay: true,
+  animationData: pika.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 const router = createBrowserRouter(
   [
@@ -25,8 +37,41 @@ const router = createBrowserRouter(
 )
 
 function App() {
-  return <RouterProvider router={router}/>
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(undefined);
+  const [completed, setCompleted] = useState(undefined);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(
+        ""
+      )
+        .then((response) => response.ok)
+        .then((outBuff) => {
+          setData(outBuff);
+          setLoading(true);
+          setTimeout(() => {
+            setCompleted(true);
+          }, 1100);
+        });
+    }, 2000);
+  }, [data]);
 
+  return (<>
+    {!completed && !loading ? (
+      <Lottie
+        options={defaultOption1}
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        height={300}
+        width={300}
+      />
+    ) : <RouterProvider router={router}/>
+      }
+  </>);
 }
 
 export default App
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import './About.css'
-import content from '../json/content';
 
 const About = () => {
   return (
