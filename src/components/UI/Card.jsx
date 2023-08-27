a8c2a116f2bb5f6ffbdfc2d56777248af6a58d4c
Subha Sadhu <subhasadhu5@gmail.com>
2023-08-27 17:39:37 +0530

    fix: buttons repaired

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,14 +1,53 @@
-const Card = ({title, body, link, imgLink, imgDescription}) => {
+const Card = ({ title, body, link, imgLink, imgDescription }) => {
   return (
-    <div className="card" style={{maxWidth: '20rem', margin: '1rem', backgroundColor: '#423d3d', color: 'white'}}>
-      <img src={imgLink} className="card-img-top" alt={imgDescription} style={{}}/>
+    <div
+      className="card"
+      style={{
+        maxWidth: "20rem",
+        margin: "1rem",
+        backgroundColor: "#423d3d",
+        color: "white",
+      }}
+    >
+      <img
+        src={imgLink}
+        className="card-img-top"
+        alt={imgDescription}
+        style={{}}
+      />
       <div className="card-body">
-        <h5 className="card-title" style={{textAlign: 'center', fontWeight: 'bolder'}}>{title}</h5>
-        <p className="card-text" >
-          {body.map(para=><p style={{marginBottom: "1rem", fontSize: "1rem", fontFamily: "sans-serif"}} key={para}>{para}</p>)}
+        <h5
+          className="card-title"
+          style={{ textAlign: "center", fontWeight: "bolder" }}
+        >
+          {title}
+        </h5>
+        <p className="card-text">
+          {body.map((para) => (
+            <p
+              style={{
+                marginBottom: "1rem",
+                fontSize: "1rem",
+                fontFamily: "sans-serif",
+              }}
+              key={para}
+            >
+              {para}
+            </p>
+          ))}
         </p>
-        <button disabled={link.trim().length===0} href={link} className="btn btn-secondary" style={{boxShadow: 'inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255), 0.3em 0.3em 1em rgba(0, 0, 0, 0.3)'}}>
-          {link.trim().length ? 'Go to Event' : 'Registrations opening soon'}
+        <button
+          disabled={link.trim().length === 0}
+          href={link}
+          className="btn btn-secondary"
+          style={{
+            boxShadow:
+              "inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255), 0.3em 0.3em 1em rgba(0, 0, 0, 0.3)",
+          }}
+        >
+          <a href={link} style={{color: 'white', textDecoration: 'none'}}>
+            {link.trim().length ? "Go to Event" : "Registrations opening soon"}
+          </a>
         </button>
       </div>
     </div>
