191ff67824212379fa7c2281fd7ce1a9cd7545e7
Subha-5 <subhasadhu5@gmail.com>
2023-09-26 02:43:36 +0530

    akimatsuri 2023 event concluded

---
+++ src/components/EventEnd.jsx
@@ -0,0 +1,10 @@
+const EventEnd = () => {
+  return (
+    <div style={{background: 'yellow', fontFamily: 'Poppins', fontSize: '2vw', padding: '0.75rem', textAlign: 'center', color: 'grey', fontWeight: 'bolder'}}>
+        <p>Event successfully concluded on 19th September, 2023.</p>
+        <p>Thank you for your cooperation.</p>
+    </div>
+  )
+}
+
+export default EventEnd
