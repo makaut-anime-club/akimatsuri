0645807a7c1059b3a88b7330970f62a71a714c42
makaut-anime-club <139333062+makaut-anime-club@users.noreply.github.com>
2024-08-07 13:21:52 +0530

    Update EventEnd.jsx

--- src/components/EventEnd.jsx
+++ src/components/EventEnd.jsx
@@ -1,7 +1,7 @@
 const EventEnd = () => {
   return (
     <div style={{background: 'yellow', fontFamily: 'Poppins', fontSize: '2vw', padding: '0.75rem', textAlign: 'center', color: 'grey', fontWeight: 'bolder'}}>
-        <p>Event successfully concluded on 19th September, 2023.</p>
+        <p>2024 event upcoming.</p>
         <p>Thank you for your cooperation.</p>
     </div>
   )
